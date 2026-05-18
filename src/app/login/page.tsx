72bd3909e4793a8d9c1aa7b1df614af95cbc5818
JustReueben44 <reubenwaysome@outlook.com>
2026-05-18 15:12:28 +0100

    callback

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'azure',
         options: {
-            redirectTo: `${window.location.origin}/search`
+            redirectTo: `${window.location.origin}/auth/callback`
         }
     });
     if (error) {
