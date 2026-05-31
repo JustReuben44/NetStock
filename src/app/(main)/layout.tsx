b8cf36bb0a69acbb9d33d82f52690df022a367c7
Reuben Waysome <reuben.waysome@city.ac.uk>
2026-05-31 10:05:02 +0100

    Save

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -46,22 +46,50 @@ export default function MainLayout({
 
 
 <div style={{ display: "flex", flexGrow: 1 }}>
+  
 <div className="sidebar" id="sidebar" style={{ display: isSidebarOpen ? 'flex' : 'none' }}>
   <div>
-    <div style={{ display: "flex", flex: 1, gap: "0.5rem", alignItems: "center"}}>
+    <div style={{ display: "flex", flex: 1, gap: "0.25rem", alignItems: "center"}}>
       <h1> <a href="/"> <span className="brand"> // </span> Netstock</a></h1>
+      <button className="menu" onClick={toggleSidebar} style={{display: "flex", alignItems: "center" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width="28" height="28">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
     </div>
   </div>
-  <button className="sidebar-link" onClick={toggleSidebar} style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}> Close</button>
-  <a className="sidebar-link" href="#">About</a>
-  <a className="sidebar-link" href="#">Services</a>
-  <a className="sidebar-link" href="#">Clients</a>
-  <a className="sidebar-link" href="#">Contact</a>
+
+  <ul className="sidebarContent">
+    <li className="sidebarItem">
+      <a className="sidebarContent" href="/" style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+      </svg>
+      Search
+      </a>
+    </li>
+    <li className="sidebarItem">
+      <a className="sidebarContent" href="/basket" style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+        </svg>
+        Basket
+      </a>
+    </li>
+    <li className="sidebarItem">
+      <a className="sidebarContent" href="#" style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+        </svg>
+        My Borrowed Items
+      </a>
+    </li>
+  </ul>
+  
 </div>
-      
-    
-  <main style={{flexGrow: 1}}>{children}</main>
-  </div>
-    </>
-  );
+
+<main style={{flexGrow: 1}}>{children}</main>
+</div>
+</>
+);
 }
